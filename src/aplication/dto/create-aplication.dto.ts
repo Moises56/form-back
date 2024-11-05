8d8e2051b0027528806d0d0e13a1d01a5d860a7f
moises0814 <moises.aviles@amdc.hn>
2024-11-05 15:15:11 -0600

    db

--- src/aplication/dto/create-aplication.dto.ts
+++ src/aplication/dto/create-aplication.dto.ts
@@ -8,70 +8,71 @@ import {
 } from 'class-validator';
 
 export class CreateAplicationDto {
-  @IsString() fullName: string;
-  @IsDateString() date: string;
-  @IsString() address: string;
-  @IsString() city: string;
-  @IsString() apartmentUnit: string;
-  @IsString() state: string;
-  @IsString() zipCode: string;
-  @IsString() phone: string;
-  @IsEmail() email: string;
-  @IsDateString() dateOfBirth: string;
-  @IsString() socialSecurity: string;
-  @IsNumber() salaryDesired: number;
-  @IsDateString() startDate: string;
-  @IsString() positionApplied: string;
-  @IsString() equipmentExperience: string;
+  @IsOptional() @IsString() fullName?: string;
+  @IsOptional() @IsDateString() date?: string;
+  @IsOptional() @IsString() address?: string;
+  @IsOptional() @IsString() city?: string;
+  @IsOptional() @IsString() apartmentUnit?: string;
+  @IsOptional() @IsString() state?: string;
+  @IsOptional() @IsString() zipCode?: string;
+  @IsOptional() @IsString() phone?: string;
+  @IsOptional() @IsEmail() email?: string;
+  @IsOptional() @IsDateString() dateOfBirth?: string;
+  @IsOptional() @IsString() socialSecurity?: string;
+  @IsOptional() @IsNumber() salaryDesired?: number;
+  @IsOptional() @IsDateString() startDate?: string;
+  @IsOptional() @IsString() positionApplied?: string;
+  @IsOptional() @IsString() equipmentExperience?: string;
 
   // Legalities
-  @IsBoolean() authorizedToWork: boolean;
-  @IsBoolean() legalProofProvided: boolean;
-  @IsBoolean() previousEmployee: boolean;
+  @IsOptional() @IsBoolean() authorizedToWork?: boolean;
+  @IsOptional() @IsBoolean() legalProofProvided?: boolean;
+  @IsOptional() @IsBoolean() previousEmployee?: boolean;
   @IsOptional() @IsDateString() previousEmploymentDate?: string;
-  @IsBoolean() convictedFelony: boolean;
-  @IsBoolean() onParoleOrProbation: boolean;
+  @IsOptional() @IsBoolean() convictedFelony?: boolean;
+  @IsOptional() @IsBoolean() onParoleOrProbation?: boolean;
   @IsOptional() @IsString() paroleOfficerName?: string;
   @IsOptional() @IsString() paroleOfficerPhone?: string;
-  @IsBoolean() transportation: boolean;
-  @IsBoolean() validDriverLicense: boolean;
+  @IsOptional() @IsBoolean() transportation?: boolean;
+  @IsOptional() @IsBoolean() validDriverLicense?: boolean;
   @IsOptional() @IsString() driverLicenseNumber?: string;
   @IsOptional() @IsString() driverLicenseState?: string;
   @IsOptional() @IsDateString() driverLicenseExpDate?: string;
-  @IsBoolean() accidentsLast3Years: boolean;
-  @IsNumber() accidentCount: number;
-  @IsBoolean() violationsLast3Years: boolean;
-  @IsNumber() violationCount: number;
-  @IsBoolean() trailerProficiency: string;
-  @IsBoolean() drugTestConsent: boolean;
+  @IsOptional() @IsBoolean() accidentsLast3Years?: boolean;
+  @IsOptional() @IsNumber() accidentCount?: number;
+  @IsOptional() @IsBoolean() violationsLast3Years?: boolean;
+  @IsOptional() @IsNumber() violationCount?: number;
+  @IsOptional() @IsString() trailerProficiency?: string;
+  @IsOptional() @IsBoolean() drugTestConsent?: boolean;
 
   // Employment Preferences
-  @IsString() hoursDesired: string;
+  @IsOptional() @IsString() hoursDesired?: string;
   @IsOptional() @IsString() daysUnavailable?: string;
-  @IsBoolean() willingToWorkOvertime: boolean;
+  @IsOptional() @IsBoolean() willingToWorkOvertime?: boolean;
+  @IsOptional() @IsString() ListdaysUnavailable?: string;
 
   // Emergency Contact
-  @IsString() emergencyContactName: string;
-  @IsString() emergencyContactPhone: string;
-  @IsString() emergencyContactRelation: string;
-  @IsBoolean() isOver18: boolean;
-  @IsString() gender: string;
-  @IsString() maritalStatus: string;
-  @IsString() ethnicity: string;
+  @IsOptional() @IsString() emergencyContactName?: string;
+  @IsOptional() @IsString() emergencyContactPhone?: string;
+  @IsOptional() @IsString() emergencyContactRelation?: string;
+  @IsOptional() @IsBoolean() isOver18?: boolean;
+  @IsOptional() @IsString() gender?: string;
+  @IsOptional() @IsString() maritalStatus?: string;
+  @IsOptional() @IsString() ethnicity?: string;
 
   // Previous Employment
-  @IsString() previousCompany: string;
-  @IsString() previousCompanyPhone: string;
-  @IsString() previousCompanyAddress: string;
-  @IsString() previousSupervisor: string;
-  @IsString() previousJobTitle: string;
-  @IsNumber() startingSalary: number;
-  @IsNumber() endingSalary: number;
-  @IsString() previousResponsibilities: string;
-  @IsDateString() previousEmploymentFrom: string;
-  @IsDateString() previousEmploymentTo: string;
-  @IsString() reasonForLeaving: string;
-  @IsBoolean() canContactSupervisor: boolean;
+  @IsOptional() @IsString() previousCompany?: string;
+  @IsOptional() @IsString() previousCompanyPhone?: string;
+  @IsOptional() @IsString() previousCompanyAddress?: string;
+  @IsOptional() @IsString() previousSupervisor?: string;
+  @IsOptional() @IsString() previousJobTitle?: string;
+  @IsOptional() @IsNumber() startingSalary?: number;
+  @IsOptional() @IsNumber() endingSalary?: number;
+  @IsOptional() @IsString() previousResponsibilities?: string;
+  @IsOptional() @IsDateString() previousEmploymentFrom?: string;
+  @IsOptional() @IsDateString() previousEmploymentTo?: string;
+  @IsOptional() @IsString() reasonForLeaving?: string;
+  @IsOptional() @IsBoolean() canContactSupervisor?: boolean;
 
   // Military Service
   @IsOptional() @IsString() militaryBranch?: string;
@@ -79,8 +80,8 @@ export class CreateAplicationDto {
   @IsOptional() @IsDateString() militaryTo?: string;
 
   // Identification
-  @IsString() identificationType: string;
-  @IsString() identificationImage: string;
-  @IsNumber() applicationStatus: number;
-  @IsString() publicId: string;
+  @IsOptional() @IsString() identificationType?: string;
+  @IsOptional() @IsString() identificationImage?: string;
+  @IsOptional() @IsNumber() applicationStatus?: number;
+  @IsOptional() @IsString() publicId?: string;
 }
